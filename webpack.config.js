99d1eb879b73fa4c76e8dd3f454cd1faa9d9e89b
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2023-01-17 11:15:14 -0300

    Added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  output: {
+    directory: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [ new HtmlWebpackPlugin({ template: "src/ui/index.html" }) ],
+  devServer: { port: 3000 },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)/,
+        use: ["babel-loader"],
+        exclude: "/node_modules/"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["js", "jsx", "ts", "tsx"]
+  }
+}
