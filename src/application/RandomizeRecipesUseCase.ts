9bf0cab408ed05af6a3c436279b5af9e699514e9
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2023-01-20 21:11:22 -0300

    Adapted Modified RandomizeRecipesUseCase.ts to the change of domain entities

--- src/application/RandomizeRecipesUseCase.ts
+++ src/application/RandomizeRecipesUseCase.ts
@@ -2,10 +2,15 @@ import Recipe, { RecipeType } from "../domain/Recipe";
 import IUseCase from "./IUseCase";
 
 export default class RandomizeRecipesUseCase implements IUseCase {
-  execute(recipes: Recipe[], date: Date): void {
-    const weekRecipes = recipes.filter((r) => r.type == RecipeType.Week)
-    const weekendRecipes = recipes.filter((r) => r.type == RecipeType.Weekend)
+  execute(recipes: Recipe[], date: Date): [Recipe, Date][] {
+    const weekRecipes = recipes.filter(
+      r => r.type === RecipeType.Week || r.type === RecipeType.Both
+    )
+    const weekendRecipes = recipes.filter(
+      r => r.type === RecipeType.Weekend || r.type === RecipeType.Both
+    )
 
+    const recipesWithDates: [Recipe, Date][] = []
     const currentDate = date;
 
     while (weekRecipes.length !== 0 || weekendRecipes.length !== 0) {
@@ -15,22 +20,22 @@ export default class RandomizeRecipesUseCase implements IUseCase {
         if (weekendRecipes.length !== 0) {
           let randomIndex = Math.floor(Math.random() * weekendRecipes.length)
           const [chosenRecipe] = weekendRecipes.splice(randomIndex, 1)
-          // recipes[recipes.indexOf(chosenRecipe)].date = currentDate
           const originalIndex = recipes.findIndex(r => r.id === chosenRecipe.id)
-          recipes[originalIndex].date = new Date(currentDate)
+          recipesWithDates.push([recipes[originalIndex], new Date(currentDate)])
+
         }
       } else { // Weekdays
         if (weekRecipes.length !== 0) {
           let randomIndex = Math.floor(Math.random() * weekRecipes.length)
           const [chosenRecipe] = weekRecipes.splice(randomIndex, 1)
           const originalIndex = recipes.findIndex(r => r.id === chosenRecipe.id)
-          recipes[originalIndex].date = new Date(currentDate)
+          recipesWithDates.push([recipes[originalIndex], new Date(currentDate)])
         }
       }
 
       currentDate.setDate(currentDate.getDate() + 1)
     }
 
-    console.log(recipes)
+    return recipesWithDates
   }
 }
