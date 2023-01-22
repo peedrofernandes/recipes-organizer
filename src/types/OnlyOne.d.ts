51164b7465cac6dcd8dd840bc650fad941f68137
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2023-01-22 16:34:46 -0300

    Added OnlyOne type to help solve small issues

---
+++ src/types/OnlyOne.d.ts
@@ -0,0 +1,7 @@
+import { AtLeastOne } from "./AtLeastOne";
+
+export type OnlyOne<T, U = {
+  [K1 in keyof T]: {
+    [K2 in keyof T]?: Pick<T, K1> extends Pick<T, K2> ? T[K1] : undefined
+  }
+}> = (U[keyof U]) & AtLeastOne<T>
