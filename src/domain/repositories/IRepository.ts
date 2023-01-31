bfc53e3a6f4ed13e6717849f808e8b0db315f1b8
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2023-01-31 10:43:24 -0300

    Refactored repository interface

--- src/domain/repositories/IRepository.ts
+++ src/domain/repositories/IRepository.ts
@@ -4,7 +4,7 @@ import { Id } from "../value-objects/Id";
 export interface IRepository <T> {
   find(id: Id): Promise<T>;
   create(t: T): Promise<void>;
-  create(t: T[]): Promise<void>;
+  createList(t: T[]): Promise<void>;
   update(id: Id, attributes: Attributes<T>): Promise<void>;
   delete(id: Id): Promise<void>;
   load(source: any): Promise<T[]>;
